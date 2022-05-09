905d215ac045281161ab745d8ad82690a1b40c29
soxrobjumayev <soxrobjumayev>
2022-05-09 07:03:15 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var info = {}
+
+alert("Faqat haqiqatni yozing!!!")
+info.fullName = prompt("ism familiyaninggiz")
+
+info.xato = prompt("hayotda qilgan kotta xatoyinggiz nechta", 2)
+info.qaysi = prompt("nima xato qilgansiz")
+info.moti = prompt("siqlmang wu iw bo'liwi keri ekan bo'ldi")
+info.reason = prompt("bowqa bunaqa iwlani takrorlamaslikga iwonasizmi")
+info.harakat = prompt("bowqa bunaqa xato qilmasligizga iwonamiza")
+info.sog = prompt("nega to'g'risini yozmadiz!");
+
+
+
+alert("baribir haqiqatni gapirmadiz!   bizni alday olmaysiz!   sog boling");
+console.log("haqiqat",info);
